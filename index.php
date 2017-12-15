8b2b058e66fd9612a5e070f36bbb9882a7db997e
rafaucau <calka.rafal@gmail.com>
2017-12-15 22:29:08 +0100

    frontend
    Wyczyszczenie kodu ze zbędnych i zdeprecojowanych rzeczy

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE HTML>
+<html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <meta charset="utf-8" >
+    <link href="css/style.css" rel="stylesheet">
     <link rel="icon" href="img/favicon.ico">
     <title>ROS - Restaurant Ordering System</title>
-    <script type="text/javascript" src="scripts.js"></script>
-    </head>
+    <script src="scripts.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lato&amp;subset=latin-ext" rel="stylesheet"><!--czcionka-->
+</head>
 
-    
-<body onload="counter();">
-    
-    <header>
-        <img src="img/restaurant.png" alt="Restaurant Ordering System" />
-    </header>
-    
-    <div id="buttons">
-        <a href="collect.php"><button>Collect</button></a>
-        <a href="orders.php"><button>Orders</button></a>
-        <a href="foodstuffs.php"><button>Foodstuffs</button></a>
-        <a href="reports.php"><button>Reports</button></a>
-    </div>
-    
-    <div id="clock"></div>
-    <footer>
-        <h2>Developed by: Szymon Kulikowski</h2>
-    </footer>
+<body>
+    <main class="container">
+        <header>
+            <img src="img/restaurant.png" alt="Restaurant Ordering System">
+        </header>
+        
+        <div id="buttons">
+            <a href="collect.php"><button id="button1">Collect</button></a>
+            <a href="orders.php"><button id="button2">Orders</button></a>
+            <a href="foodstuffs.php"><button id="button3">Foodstuffs</button></a>
+            <a href="reports.php"><button id="button4">Reports</button></a>
+        </div>
+        
+        <div id="clock"></div>
+        <script>counter();</script>
+    </main>
     
 </body>
     
